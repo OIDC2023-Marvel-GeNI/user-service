b45bb4df408b8d5a36a4b8484491374ec624410e
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2023-07-09 20:20:57 +0900

    set main

---
+++ src/test/java/marvel/geni/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package marvel.geni.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
